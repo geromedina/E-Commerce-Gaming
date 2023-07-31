6b3a6856025dfb196d4023ce96be21412e75aa5b
geromedina <gerocode768@gmail.com>
2023-07-31 20:09:47 -0300

    register form

--- client/src/pages/join/sign-up/sign-up.js
+++ client/src/pages/join/sign-up/sign-up.js
@@ -1,13 +1,20 @@
 import React from "react";
 import { JoinLayout } from "@/layouts";
 import styles from "./sign-up.module.scss";
+import Link from "next/link";
+import { RegisterForm } from "@/components/Auth";
 
 export default function SignUpPage() {
     return (
         <>
             <JoinLayout>
-                <div>
+                <div className={styles.signUp}>
                     <h3>Crear cuenta</h3>
+                    <RegisterForm />
+
+                    <div className={styles.actions}>
+                        <Link href="/join/sign-in">Atras</Link>
+                    </div>
                 </div>
             </JoinLayout>
         </>
