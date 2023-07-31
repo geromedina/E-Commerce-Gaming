39b4c71557f4157491e40f8e94e7d64858e334a0
geromedina <gerocode768@gmail.com>
2023-07-31 16:22:12 -0300

    añado estilos scss

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { Button } from "semantic-ui-react";
 
 export default function Index() {
     return (
         <div>
             <h2>Games Shop</h2>
+            <Button primary>Ir al Login</Button>
         </div>
     );
 }
